4812ad6ce4466b428925364caf2817821ad7f4a9
DanieleM1999 <danielemirauta@gmail.com>
2020-06-23 16:43:11 +0200

    Risoluzione problemi service e repository.

--- src/main/java/it/spring/configuration/WebAppInitializer.java
+++ src/main/java/it/spring/configuration/WebAppInitializer.java
@@ -4,10 +4,17 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
 public class WebAppInitializer implements WebApplicationInitializer{
 	
 	@Override
